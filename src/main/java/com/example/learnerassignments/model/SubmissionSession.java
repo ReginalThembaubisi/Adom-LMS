bed900865983e489a7d363d8b7985b28d90f14c4
Reginal Thembaubisi <ReginalThembaubisi@users.noreply.github.com>
2026-08-14 10:09:50 +0200

    fix: revert SastToUtcConverter and use native spring.jpa.properties.hibernate.jdbc.time_zone property instead to avoid double timezone shifting and fix old/new sessions

--- src/main/java/com/example/learnerassignments/model/SubmissionSession.java
+++ src/main/java/com/example/learnerassignments/model/SubmissionSession.java
@@ -30,12 +30,10 @@ public class SubmissionSession {
 
     @NotNull(message = "Start time is required")
     @Column(name = "start_time", nullable = false)
-    @Convert(converter = com.example.learnerassignments.converter.SastToUtcConverter.class)
     private LocalDateTime startTime;
 
     @NotNull(message = "End time is required")
     @Column(name = "end_time", nullable = false)
-    @Convert(converter = com.example.learnerassignments.converter.SastToUtcConverter.class)
     private LocalDateTime endTime;
 
     @Enumerated(EnumType.STRING)
