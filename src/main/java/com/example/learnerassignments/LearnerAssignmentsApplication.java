5c1de9838db39826d6edb1ea8a9aa299ab65af91
Reginal Thembaubisi <ReginalThembaubisi@users.noreply.github.com>
2026-08-18 08:29:41 +0200

    Inject database URL parsing at application startup (main method) and delete redundant postprocessor class

--- src/main/java/com/example/learnerassignments/LearnerAssignmentsApplication.java
+++ src/main/java/com/example/learnerassignments/LearnerAssignmentsApplication.java
@@ -18,6 +18,27 @@ public class LearnerAssignmentsApplication {
 	}
 
 	public static void main(String[] args) {
+		String dbUrl = System.getenv("SPRING_DATASOURCE_URL");
+		if (dbUrl != null && (dbUrl.startsWith("postgres://") || dbUrl.startsWith("postgresql://"))) {
+			String prefix = dbUrl.startsWith("postgres://") ? "postgres://" : "postgresql://";
+			String rawUrl = dbUrl.substring(prefix.length());
+			int atIndex = rawUrl.indexOf("@");
+			if (atIndex != -1) {
+				String credentials = rawUrl.substring(0, atIndex);
+				String hostAndDb = rawUrl.substring(atIndex + 1);
+				
+				String[] credParts = credentials.split(":");
+				String username = credParts[0];
+				String password = credParts.length > 1 ? credParts[1] : "";
+				
+				String jdbcUrl = "jdbc:postgresql://" + hostAndDb;
+				
+				System.setProperty("spring.datasource.url", jdbcUrl);
+				System.setProperty("spring.datasource.username", username);
+				System.setProperty("spring.datasource.password", password);
+				System.out.println("LearnerAssignmentsApplication: Auto-configured connection URL to jdbc:postgresql:// format");
+			}
+		}
 		SpringApplication.run(LearnerAssignmentsApplication.class, args);
 	}
 
